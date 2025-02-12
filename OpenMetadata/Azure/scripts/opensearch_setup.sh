6062f776ae0819688188664e7df1f6d813e8260f
Michael Haigh <mhaigh@netapp.com>
2025-02-12 17:23:21 -0500

    fixing single quote local-exec variable issue
    Signed-off-by: Michael Haigh <mhaigh@netapp.com>

--- OpenMetadata/Azure/scripts/opensearch_setup.sh
+++ OpenMetadata/Azure/scripts/opensearch_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -X PUT -u icopensearch:${os_ic_password} ${host}/_plugins/_security/api/internalusers/openmetadata -H 'Content-Type: application/json' -d'
+curl -X PUT -u icopensearch:${os_ic_password} ${host}/_plugins/_security/api/internalusers/openmetadata -H 'Content-Type: application/json' -d"
 {
-    "password": "${os_om_password}",
-    "backend_roles": [],
-    "attributes": {}
-}'
+    \"password\": \"${os_om_password}\",
+    \"backend_roles\": [],
+    \"attributes\": {}
+}"
 curl -X PUT -u icopensearch:${os_ic_password} ${host}/_plugins/_security/api/rolesmapping/all_access -H 'Content-Type: application/json' -d'
 {
     "users" : [ "openmetadata" ]
